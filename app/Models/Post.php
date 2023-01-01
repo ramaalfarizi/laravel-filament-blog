307d13dd1764e6054f4edd1bfdf7f153727b1893
ramaalfarizi <alfrzrmdhni@gmail.com>
2023-01-01 15:58:04 +0700

    add posts and tags

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Post extends Model
+class Post extends Model  implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = ['category_id', 'title', 'slug', 'content', 'is_published'];
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
 
     public function category()
     {
